852085f19233954e91d1f8c49c8f8205016fcbfc
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-01-21 21:53:50 -0500

    deleteling routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,14 +17,8 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path='/' element={<HomePage />} />
-
           <Route path="/pizza" element={<PizzaPage />} />
-          <Route path="/pizza-add" element={<PizzaFormPage />} />
-          <Route path="/pizza-add/:id" element={<PizzaFormPage />} />
-
           <Route path='/ingredient' element={<IngredientPage/>}/>
-          <Route path='/ingredient-add' element={<IngredientFormPage/>}/>
-          <Route path='/ingredient-add/:id' element={<IngredientFormPage/>}/>
         </Routes>
       </Router>
     </PizzaProvider>
